fb978b157fbab07902fafcf104f1630a8d5bf8a4
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-11-28 18:18:24 +0000

    use file-scoped namespaces

--- Enhancer/__ReadMe_Decompilers.cs
+++ Enhancer/__ReadMe_Decompilers.cs
@@ -1,13 +1,12 @@
-namespace Readme
-{
-    internal class DecompilerMessage
-    {
-        const string Hello = "The source code for this project is freely available on request, btw.";
+namespace Readme;
 
-        internal DecompilerMessage()
-        {
-            if (Hello.Length == 69)
-                return;
-        }
+internal class DecompilerMessage
+{
+    const string Hello = "The source code for this project is freely available on request, btw.";
+
+    internal DecompilerMessage()
+    {
+        if (Hello.Length == 69)
+            return;
     }
 }
