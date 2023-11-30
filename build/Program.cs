0039deb64238ac03fc16fb4396b0ddb1d615e2b0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-11-30 13:53:47 +0000

    Patch properties files on GitHub release, run  `tcli` commands from tasks

--- build/Program.cs
+++ build/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Largely derived from https://github.com/BepInEx/BepInEx/blob/master/build/Program.cs
 
+using System.Configuration;
+using Build;
 using Cake.Common;
 using Cake.Common.IO;
+using Cake.Common.Tools.Command;
 using Cake.Common.Tools.DotNet;
 using Cake.Common.Tools.DotNet.Build;
+using Cake.Common.Tools.DotNet.MSBuild;
 using Cake.Core;
 using Cake.Core.Diagnostics;
 using Cake.Core.IO;
@@ -22,10 +26,17 @@ public class BuildContext : FrostingContext
     {
         RootDirectory = ctx.Environment.WorkingDirectory.GetParent();
         DistributionDirectory = RootDirectory.Combine("dist");
-        BuildType = ctx.Argument("build-type", ProjectBuildType.Development);
-        var props = Project.FromFile(RootDirectory.CombineWithFilePath("Directory.Build.props").FullPath, new ProjectOptions());
-        VersionPrefix = props.GetPropertyValue("VersionPrefix");
+        ThunderstoreConfigFile = RootDirectory.Combine("assets").CombineWithFilePath("thunderstore.toml");
         CurrentCommit = ctx.GitLogTip(RootDirectory);
+        
+        SolutionBuildProperties = Project.FromFile(
+            RootDirectory.CombineWithFilePath("Directory.Build.props").FullPath,
+            new ProjectOptions()
+        );
+        SolutionThunderstoreProperties = ThunderstoreProject.FromFile(ThunderstoreConfigFile.FullPath);
+        
+        BuildType = ctx.Argument("build-type", ProjectBuildType.Development);
+        GitHubReleaseTagName = ctx.Argument<string>("github-tag-name", "");
     }
 
     public enum ProjectBuildType
@@ -35,11 +46,15 @@ public class BuildContext : FrostingContext
     }
     
     public ProjectBuildType BuildType { get; }
+    public string GitHubReleaseTagName { get; }
     
     public DirectoryPath RootDirectory { get; }
     public DirectoryPath DistributionDirectory { get; }
+    public FilePath ThunderstoreConfigFile { get; }
     
-    public string VersionPrefix { get; }
+    public Project SolutionBuildProperties { get; }
+    public ThunderstoreProject SolutionThunderstoreProperties { get; }
+    public string VersionPrefix => SolutionBuildProperties.GetPropertyValue("VersionPrefix");
     public GitCommit CurrentCommit { get; }
     
     public string VersionSuffix => BuildType switch
@@ -54,8 +69,6 @@ public class BuildContext : FrostingContext
         ProjectBuildType.Release => "",
         _                        => $"-{VersionSuffix}+{this.GitShortenSha(RootDirectory, CurrentCommit)}",
     };
-
-    public string TargetZipName(string targetName) => $"AugmentedEnhancer-{BuildPackageVersion}-{targetName}.zip";
 }
 
 [TaskName("Clean")]
@@ -72,31 +85,116 @@ public sealed class CleanTask : FrostingTask<BuildContext>
     }
 }
 
+[TaskName("PatchBuildProps")]
+public sealed class PatchBuildPropsTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext ctx) => 
+        !string.IsNullOrWhiteSpace(ctx.GitHubReleaseTagName) && ctx.GitHubReleaseTagName != ctx.VersionPrefix;
+
+    public override void Run(BuildContext ctx)
+    {
+        ctx.SolutionBuildProperties.SetProperty("VersionPrefix", ctx.GitHubReleaseTagName);
+        ctx.SolutionBuildProperties.Save();
+    }
+}
+
 [TaskName("Compile")]
 [IsDependentOn(typeof(CleanTask))]
+[IsDependentOn(typeof(PatchBuildPropsTask))]
 public sealed class CompileTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext ctx)
+    public DotNetBuildSettings DetermineBuildSettings(BuildContext ctx)
     {
         var buildSettings = new DotNetBuildSettings
         {
             Configuration = "Release"
         };
-        
-        if (ctx.BuildType != BuildContext.ProjectBuildType.Release)
-        {
-            buildSettings.MSBuildSettings = new()
-            {
-                VersionSuffix = ctx.VersionSuffix,
-                Properties =
-                {
-                    ["SourceRevisionId"] = new[] { ctx.CurrentCommit.Sha },
-                    ["RepositoryBranch"] = new[] { ctx.GitBranchCurrent(ctx.RootDirectory).FriendlyName }
-                }
-            };
-        }
+        ConfigureDevelopmentMetadata(ctx, buildSettings);
+        return buildSettings;
+    }
 
-        ctx.DotNetBuild(ctx.RootDirectory.FullPath, buildSettings);
+    public void ConfigureDevelopmentMetadata(BuildContext ctx, DotNetBuildSettings buildSettings)
+    {
+        if (ctx.BuildType == BuildContext.ProjectBuildType.Release) return;
+        
+        buildSettings.MSBuildSettings = new DotNetMSBuildSettings
+        {
+            VersionSuffix = ctx.VersionSuffix,
+            Properties =
+            {
+                ["SourceRevisionId"] = new[] { ctx.CurrentCommit.Sha },
+                ["RepositoryBranch"] = new[] { ctx.GitBranchCurrent(ctx.RootDirectory).FriendlyName }
+            }
+        };
+    }
+
+    public override void Run(BuildContext ctx)
+    {
+        ctx.DotNetBuild(ctx.RootDirectory.FullPath, DetermineBuildSettings(ctx));
+    }
+}
+
+[TaskName("PatchThunderstoreMetadata")]
+public sealed class PatchThunderstoreMetadataTask : FrostingTask<BuildContext>
+{
+    public override bool ShouldRun(BuildContext ctx) =>
+        ctx.SolutionThunderstoreProperties.Package?.VersionNumber != ctx.VersionPrefix;
+
+    public override void Run(BuildContext ctx)
+    {
+        if (ctx.SolutionThunderstoreProperties.Package is null) throw new ConfigurationErrorsException();
+        ctx.SolutionThunderstoreProperties.Package!.VersionNumber = ctx.VersionPrefix;
+        ctx.SolutionThunderstoreProperties.ToFile(ctx.ThunderstoreConfigFile);
+    }
+}
+
+[TaskName("BuildThunderstore")]
+[IsDependentOn(typeof(CompileTask))]
+[IsDependentOn(typeof(PatchThunderstoreMetadataTask))]
+public sealed class BuildThunderstoreTask : FrostingTask<BuildContext>
+{
+    public void RenameBuildArchive(BuildContext ctx)
+    {
+        if (ctx.BuildType == BuildContext.ProjectBuildType.Release) return;
+
+        var oldBuildArchiveName = ctx.DistributionDirectory
+            .CombineWithFilePath(ctx.SolutionThunderstoreProperties.GetBuildArchiveFilename());
+        var newBuildArchiveName = ctx.DistributionDirectory
+            .CombineWithFilePath(ctx.SolutionThunderstoreProperties.GetBuildArchiveFilename(ctx.BuildPackageVersion));
+
+        ctx.MoveFile(oldBuildArchiveName, newBuildArchiveName);
+    }
+
+    public override void Run(BuildContext ctx)
+    {
+        ctx.CreateDirectory(ctx.DistributionDirectory);
+        ctx.CleanDirectory(ctx.DistributionDirectory);
+        
+        ctx.Command(
+            new []{ "tcli", "tcli.exe" },
+            new ProcessArgumentBuilder()
+                .Append("build")
+                .AppendSwitch("--config-path", ctx.ThunderstoreConfigFile.FullPath),
+            expectedExitCode: 0
+        );
+
+        RenameBuildArchive(ctx);
+    }
+}
+
+[TaskName("PublishThunderstore")]
+[IsDependentOn(typeof(BuildThunderstoreTask))]
+public sealed class PublishTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext ctx)
+    {
+        ctx.Command(
+            new []{ "tcli", "tcli.exe" },
+            new ProcessArgumentBuilder()
+                .Append("publish")
+                .AppendSwitch("--config-path", ctx.ThunderstoreConfigFile.FullPath),
+            expectedExitCode: 0
+        );
     }
 }
 
