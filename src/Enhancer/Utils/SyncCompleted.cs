ebd1304a16bbca9fdb04c9324ef799fb3f3f6383
Robyn <rnixon98@gmail.com>
2024-08-26 18:33:20 -0500

    feat: keep track of sync state for clients

--- src/Enhancer/Utils/SyncCompleted.cs
+++ src/Enhancer/Utils/SyncCompleted.cs
@@ -1,31 +1,15 @@
 using System;
-using HarmonyLib;
-using Lethal_Company_Enhancer.Patches;
 
 namespace Lethal_Company_Enhancer.Utils;
 
 public static class SyncCompleted
 {
-    public static bool HasAlreadyPatched { get; private set; } = false;
+
+    internal static bool Completed = false;
 
     public static void SyncCompletedCallback(object Sender, EventArgs e)
     {
-
-        if (HasAlreadyPatched || !Plugin.Cfg.Enabled)
-        {
-            Plugin.Log.LogInfo("Globally disabled or already patched, exiting");
-            return;
-        }
-
-        Harmony patcher = new(LCMPluginInfo.PLUGIN_GUID);
-
-        Plugin.Log.LogInfo($"Sync complete, patching now");
-        patcher.PatchAll(typeof(HangarShipDoorPatches));
-        patcher.PatchAll(typeof(RoundManagerPatches));
-        patcher.PatchAll(typeof(StartOfRoundPatches));
-        patcher.PatchAll(typeof(TerminalPatches));
-        patcher.PatchAll(typeof(TimeOfDayPatches));
-
-        HasAlreadyPatched = true;
+        Plugin.Log.LogInfo("Config sync'd");
+        Completed = true;
     }
 }
