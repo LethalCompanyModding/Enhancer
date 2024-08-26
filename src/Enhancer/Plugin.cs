8936b8e5fa8e35cb77fdd626880c135134f704b6
Robyn <rnixon98@gmail.com>
2024-08-26 18:33:10 -0500

    fix: Quota days not working by patching all methods at start

--- src/Enhancer/Plugin.cs
+++ src/Enhancer/Plugin.cs
@@ -13,7 +13,9 @@
 
 using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
 using Lethal_Company_Enhancer.Config;
+using Lethal_Company_Enhancer.Patches;
 using Lethal_Company_Enhancer.Utils;
 
 [BepInDependency("com.sigurd.csync", "5.0.1")]
@@ -33,7 +35,17 @@ public class Plugin : BaseUnityPlugin
         // Plugin startup logic
         Log = Logger;
 
+        Config.SaveOnConfigSet = false;
         Cfg = new(base.Config);
+
+        Harmony patcher = new(LCMPluginInfo.PLUGIN_GUID);
+
+        Log.LogInfo($"Patching now");
+        patcher.PatchAll(typeof(HangarShipDoorPatches));
+        patcher.PatchAll(typeof(RoundManagerPatches));
+        patcher.PatchAll(typeof(StartOfRoundPatches));
+        patcher.PatchAll(typeof(TerminalPatches));
+        patcher.PatchAll(typeof(TimeOfDayPatches));
 
         Logger.LogInfo("Attaching a sync listener");
         Cfg.InitialSyncCompleted += SyncCompleted.SyncCompletedCallback;
