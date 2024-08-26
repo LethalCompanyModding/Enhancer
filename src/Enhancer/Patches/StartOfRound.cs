72cb923e0dd3234f1fdd0d375d5007c9eeedb970
Robyn <rnixon98@gmail.com>
2024-08-26 18:33:36 -0500

    feat: unpatch all methods on failed sync state

--- src/Enhancer/Patches/StartOfRound.cs
+++ src/Enhancer/Patches/StartOfRound.cs
@@ -1,4 +1,6 @@
 using HarmonyLib;
+using Unity.Netcode;
+using Lethal_Company_Enhancer.Utils;
 
 namespace Lethal_Company_Enhancer.Patches;
 
@@ -9,6 +11,18 @@ public class StartOfRoundPatches
     public static bool StartOfRoundShipStartPre()
     {
         Plugin.Log.LogInfo("StartOfRound Start");
+
+        if (!NetworkManager.Singleton.IsServer)
+        {
+            if (!SyncCompleted.Completed)
+            {
+                Plugin.Log.LogError("Client joined without receiving sync");
+                Plugin.Cfg.Enabled.LocalValue = false;
+                Harmony harmony = new(LCMPluginInfo.PLUGIN_GUID);
+                harmony.UnpatchSelf();
+                return true;
+            }
+        }
 
         TimeOfDay.Instance.quotaVariables.deadlineDaysAmount = Plugin.Cfg.DaysPerQuota;
 
